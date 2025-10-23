e6404a7418c810b89d0d46f7a206f1d198de7b0b
Tushar Nagle <140832814+tushar93026@users.noreply.github.com>
2025-10-23 20:01:50 +0530

    2nd Commit

--- FrontEnd/src/context/AppContext.jsx
+++ FrontEnd/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://backend-jet-theta-17.vercel.app/"
     const [doctors, setDoctors] = useState([])
 
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
